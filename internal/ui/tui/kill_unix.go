8d75e7e176e2eb201644fb2f21891cde1d713ccb
Nykon Hrytsyshyn <contact@nykon.me>
2026-03-12 23:24:16 +0200

    Add imports for unix-version

--- internal/ui/tui/kill_unix.go
+++ internal/ui/tui/kill_unix.go
@@ -2,11 +2,16 @@
 
 package tui
 
+import (
+	"os/exec"
+	"syscall"
+)
+
 /*
 setSysProcAttr starts Tilt in its own process group (Setpgid: true).
 This isolates it so the terminal's raw-mode Ctrl+C key event is not
 automatically delivered to the child as SIGINT.
- */
+*/
 func setSysProcAttr(cmd *exec.Cmd) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 }
